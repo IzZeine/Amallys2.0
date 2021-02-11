86d483ebd96bee682efffd9a349c72420e2a7b3c
IzZeine <76914814+IzZeine@users.noreply.github.com>
2021-02-11 16:21:39 +0100

    Correction optimisation

--- js/navigation.js
+++ js/navigation.js
@@ -1,36 +1,6 @@
 /* IIFE Javascript
 https://developer.mozilla.org/fr/docs/Glossaire/IIFE */
 
-/*(function () {
-    // chopper les elements dans le DOM
-    var linkOpen = document.querySelector(".js-navopen");
-    var linkClose = document.querySelector(".js-navclose");
-    var mobileMenu = document.querySelector(".js-mainnav__mobile");
-  
-    // on met nos deux éléments sur lesquels on doit écouter et agir dans un tableau
-    var allLinks = [linkOpen, linkClose];
-  
-    // parcourir le tableau
-    for (var i = 0; i < allLinks.length; i++) {
-      // mettre chaque fois l'élément du tableau dans une variable (plus facile que de taper chaque fois allLinks[i])
-      var clickableItem = allLinks[i];
-  
-      // écouter les clicks
-      clickableItem.addEventListener(
-        "click",
-        function (event) {
-          // supprimer le comportement par défaut de l'événement (ici un lcik sur un lien qui va vers une ancre)
-          event.preventDefault();
-  
-          // faire un toggle de la classe (si elle est pas là > ajouter / si elle est là > supprimer)
-          mobileMenu.classList.toggle("is-shown");
-        },
-        false
-      );
-    }
-  })();
-*/
-
   (function () {
     var mobileMenu = document.querySelector(".js-mainnav__mobile");
     var x = document.querySelector(".js-btn-mobile");
@@ -40,7 +10,6 @@ https://developer.mozilla.org/fr/docs/Glossaire/IIFE */
     allLinks.push(x);
     console.log(allLinks);
 
-
     for (var i = 0; i < allLinks.length; i++) {
         // mettre chaque fois l'élément du tableau dans une variable (plus facile que de taper chaque fois allLinks[i])
         var clickableItem = allLinks[i];
@@ -49,8 +18,6 @@ https://developer.mozilla.org/fr/docs/Glossaire/IIFE */
         clickableItem.addEventListener(
           "click",
           function (event) {
-            // supprimer le comportement par défaut de l'événement (ici un lcik sur un lien qui va vers une ancre)
-            
             x.classList.toggle("change");
     
             // faire un toggle de la classe (si elle est pas là > ajouter / si elle est là > supprimer)
